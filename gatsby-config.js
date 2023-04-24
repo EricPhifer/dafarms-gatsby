0ea9fbc645dac8a2571e1ba0b3ac9f2148fccd42
Eric Phifer <ericoncode@gmail.com>
2023-04-24 10:15:33 -0600

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,67 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `D & A Farms`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://dandafarms.net`,
+    author: 'Phifer Web Solutions',
+    description: 'We provide local, non-GMO, non-hormone beef.',
   },
-  plugins: [{
-    resolve: 'gatsby-source-sanity',
-    options: {
-      "projectId": "j6prnqim",
-      "dataset": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-sitemap"]
-};
+  plugins: [
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sanity-image',
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-simple-analytics',
+      options: {
+        trackPageViews: true,
+        events: true,
+        eventsGlobal: 'sa_event',
+        // ignorePages: ["pathname"],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://dandafarms.net',
+        sitemap: 'https://dandafarms.net/sitemap/sitemap-index.xml',
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `dafarms-gatsby`,
+        short_name: `dafarms`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `./src/images/icon.webp`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
